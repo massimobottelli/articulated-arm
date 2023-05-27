18c5ade34dac98121c341639a9413b8d19a94c99
Massimo Bottelli <88494940+massimobottelli@users.noreply.github.com>
2023-05-27 14:53:14 +0200

    Add files via upload

--- prototype.py
+++ prototype.py
@@ -8,7 +8,7 @@ pygame.init()
 board_width = 500
 board_height = 250
 
-# Define the origin coords
+# Define the origin coordinates
 origin_x = 250
 origin_y = 10
 
@@ -22,15 +22,49 @@ pygame.display.set_caption("Robotic Arm Simulation")
 
 board.fill((255, 255, 255))  # Fill the board with white
 
-# Define the font for displaying coords
+# Define the font for displaying coordinates
 font = pygame.font.Font(None, 20)
 
 
 def draw_origin():
+    board.fill((255, 255, 255))  # Clear the board
+
     # Draw the origin (initial point) on the board
     pygame.draw.circle(board, (255, 0, 0), (origin_x, origin_y), 3)
     # Draw a circle centered on the origin with a radius of arm_1_length
     pygame.draw.circle(board, (0, 0, 255), (origin_x, origin_y), arm_1_length, 1)
+
+
+def get_target():
+    x, y = pygame.mouse.get_pos()
+
+    # Draw a red dot at clicked position
+    pygame.draw.circle(board, (255, 0, 0), (x, y), 3)
+
+    # Draw a circle centered on the clicked point with a radius of arm_2_length
+    pygame.draw.circle(board, (0, 0, 255), (x, y), arm_2_length, 1)
+
+    # Display the coordinates on the board near the red dot
+    display_coords(x, y, x + 10, y - 10)
+
+    return x, y
+
+
+def find_intersection(target_x, target_y):
+    dx = target_x - origin_x
+    dy = target_y - origin_y
+    d = (arm_1_length ** 2 - arm_2_length ** 2 + distance ** 2) / (2 * distance)
+    h = (arm_1_length ** 2 - d ** 2) ** 0.5
+    intersect_x1 = origin_x + (d * dx - h * dy) / distance
+    intersect_y1 = origin_y + (h * dx + d * dy) / distance
+    intersect_x2 = origin_x + (d * dx + h * dy) / distance
+    intersect_y2 = origin_y + (-h * dx + d * dy) / distance
+
+    # Draw the points of intersection
+    pygame.draw.circle(board, (255, 0, 0), (int(intersect_x1), int(intersect_y1)), 3)
+    pygame.draw.circle(board, (255, 0, 0), (int(intersect_x2), int(intersect_y2)), 3)
+
+    return intersect_x1, intersect_y1, intersect_x2, intersect_y2
 
 
 def draw_lines(x1, y1, x2, y2):
@@ -58,7 +92,7 @@ def display_angle(angle, label_x, label_y):
 
 
 def display_coords(x, y, label_x, label_y):
-    # Display the coords on the board near the red dot
+    # Display the coordinates on the board near the red dot
     text_surface = font.render(f"({x}, {y})", True, (0, 0, 0))
     text_rect = text_surface.get_rect()
     text_rect.topleft = (label_x, label_y)
@@ -88,39 +122,21 @@ while running:
                 running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:  # Left mouse button
-                board.fill((255, 255, 255))  # Clear the board
                 draw_origin()
 
-                target_x, target_y = pygame.mouse.get_pos()
-                relative_x = target_x - origin_x
-                relative_y = target_y - origin_y
-
-                # Draw a red dot at clicked position
-                pygame.draw.circle(board, (255, 0, 0), (target_x, target_y), 3)
-
-                # Draw a circle centered on the clicked point with a radius of arm_2_length
-                pygame.draw.circle(board, (0, 0, 255), (target_x, target_y), arm_2_length, 1)
-
-                # Display the coords on the board near the red dot
-                display_coords(target_x, target_y, target_x + 10, target_y - 10)
+                target_x, target_y = get_target()
 
                 # Find the points of intersection between the two circles
                 distance = ((target_x - origin_x) ** 2 + (target_y - origin_y) ** 2) ** 0.5
-                if distance <= arm_1_length + arm_2_length:  # Check if the circles intersect
-                    dx = target_x - origin_x
-                    dy = target_y - origin_y
-                    d = (arm_1_length ** 2 - arm_2_length ** 2 + distance ** 2) / (2 * distance)
-                    h = (arm_1_length ** 2 - d ** 2) ** 0.5
-                    intersect_x1 = origin_x + (d * dx - h * dy) / distance
-                    intersect_y1 = origin_y + (h * dx + d * dy) / distance
-                    intersect_x2 = origin_x + (d * dx + h * dy) / distance
-                    intersect_y2 = origin_y + (-h * dx + d * dy) / distance
 
-                    # Draw the points of intersection
-                    pygame.draw.circle(board, (255, 0, 0), (int(intersect_x1), int(intersect_y1)), 3)
-                    pygame.draw.circle(board, (255, 0, 0), (int(intersect_x2), int(intersect_y2)), 3)
+                # Check if the circles intersect
+                if distance <= arm_1_length + arm_2_length:
 
-                    # Display the coords of the points of intersection on the board
+                    # Find intersections
+
+                    intersect_x1, intersect_y1, intersect_x2, intersect_y2 = find_intersection(target_x, target_y)
+
+                    # Display the coordinates of the points of intersection on the board
                     display_coords(int(intersect_x1), int(intersect_y1), int(intersect_x1)+10, int(intersect_y1)-10)
                     display_coords(int(intersect_x2), int(intersect_y2), int(intersect_x2)+10, int(intersect_y2)-10)
 
